80d269d20c2acfba39763ce1e953d5c138efcc16
YADHU KRISHNAN <yadhu98@gmail.com>
2021-05-07 16:10:03 +0530

    Add files via upload

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react' //wherever we use jsx in the file we should import this.
+
+function MyInfo(){
+    return(
+      <div>
+      <h1>Yadhu</h1>
+      <p>My name is Yadhu Krishnan PC,Iam a computer science engineer.Currently Working on react </p>
+      <ul>
+        <li>Tawang</li>
+        <li>Spiti Valley</li>
+        <li>Hippie Island</li>
+        </ul>
+      </div>  
+    )
+  }
+
+  export  default MyInfo //exporting the function
